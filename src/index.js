c1433a12022b31702b9e0e444621b2ed24856e0f
moon <merolmen@gmail.com>
2023-09-28 23:34:24 -0600

    JSX parte 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* const mensaje =<div> Hola TIC</div>;
+
+const carrera = 'TIC';
+const alumno = 'El Altisimo'; */
+
+const alumnotic = {
+
+   nombre: 'natalia',
+   apodo: 'naty',
+   salon: 'TIC95'
+}
+
+function informacionalumno(alumnotic){
+
+   return `Alumno : ${alumnotic.nombre} de apodo : ${alumnotic.apodo}`;
+}
+
+function salon(alumnotic){
+
+  return ` : ${alumnotic.salon}`;
+}
+
+const elemento = <div>Hola {informacionalumno(alumnotic)} del salon {salon(alumnotic)}</div>;
+// const elemento = <div>En la carrera de {carrera} esta el alumno {alumno}</div>;
+// quiero que vayas y obtengas el elemento root 
+const container = document.getElementById('root');
+
+// que quiero mostrar y en donde 
+ReactDOM.render(elemento, container);
